3d64226c7e4907563f68071ac52734cf8059a259
Aadesh Shrestha <aadeshere1@gmail.com>
2024-09-12 17:03:09 +0545

    initialize new version

---
+++ db/migrate/20240912102406_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[7.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.string :grade
+
+      t.timestamps
+    end
+    add_index :students, :email, unique: true
+  end
+end
